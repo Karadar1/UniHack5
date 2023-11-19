09cbe720e2874c389a9a9d7c6872024418dae832
Adrian Karp <Adrian.Karp@ibm.com>
2023-11-19 12:03:40 +0200

    login redirects to browse page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/Signup';
 import BrowsePage from './pages/BrowsePage/BrowsePage';
 import { UserPage } from './pages/UserPage/UserPage';
+import ProductPage from './pages/ProductPage/ProductPage';
 
 function App() {
   return (
@@ -14,10 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/browse" element={<BrowsePage />} />
+        <Route path="/product" element={<ProductPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/user" element={<UserPage />} />
-
       </Routes>
       <ToastContainer />
     </>
